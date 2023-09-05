31c4fc0549fd8ba388f8938683e02eb80cc10b64
chrissuttondev <>
2023-09-05 23:16:33 +0000

    added javascript function to play audio pads

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,81 @@
 console.log("welcome to sound fi");
+
+//Javascritp function for audio pads.
+const x = document.getElementById("kick");
+
+function playPadOne() {
+    x.play();
+} 
+
+const y = document.getElementById("snare");
+
+function playPadTwo() {
+    y.play();
+}
+
+const b = document.getElementById("snare2");
+
+function playPadThree() {
+    b.play();
+}
+
+//
+const hh1 = document.getElementById("hh1");
+
+function playPadFour() {
+    hh1.play();
+}
+
+const hh2 = document.getElementById("hh2");
+
+function playPadFive() {
+    hh2.play();
+
+}
+
+const cymbol = document.getElementById("cymbol");
+
+function playPadSix() {
+    cymbol.play();
+}
+
+const vynil = document.getElementById("vynil");
+
+function playPadSeven() {
+    vynil.play();
+}
+
+const bleep1 = document.getElementById("bleep1");
+
+function playPadEight() {
+    bleep1.play();
+}
+
+const bleep2 = document.getElementById("bleep2");
+
+function playPadNine() {
+    bleep2.play();
+
+}
+
+const keys1 = document.getElementById("keys1");
+
+function playPadTen() {
+    keys1.play();
+}
+
+const keys2 = document.getElementById("keys2");
+
+function playPadEleven() {
+    keys2.play();
+}
+
+const keys3 = document.getElementById("keys3");
+
+function playPadTwelve() {
+    keys3.play();
+}
+//audio section end
+
+//form section
+
