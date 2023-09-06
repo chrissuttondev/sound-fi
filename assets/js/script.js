05c36afaff38c904922884e8e10ae6026ae459fb
chrissuttondev <>
2023-09-06 00:15:49 +0000

    added h3 and he tags to index.html and form.html respectively

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const x = document.getElementById("kick");
 
 function playPadOne() {
     x.play();
-} 
+}
 
 const y = document.getElementById("snare");
 
@@ -98,6 +98,22 @@ eg https://www.myinstants.com/en/instant/ive-got-you-in-my-sight-52194/
 
 */
 
+//form validation function 
 
-//form section
+function handleSubmit(event) {
+    event.preventDefault();
+    let p1 = form.elements['password'].value;
+    let p2 = form.elements['confirm-password'].value;
 
+    if (p1 !== p2) {
+        let errorDiv = document.getElementById('errors');
+        errorDiv.innerHTML = "<p>Please ensure your passwords match.</p>";
+        errorDiv.style.display = 'block';
+    } else {
+        console.log('Validation successful!');
+        form.submit();
+    }
+}
+
+let form = document.getElementById('registration-form');
+form.addEventListener('submit', handleSubmit);
