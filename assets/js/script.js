444d7df9d58ff9c7f2841f12d5e950a8df42e1d7
chrissuttondev <>
2023-09-06 13:32:33 +0000

    final mentor session feedback, line 2 scrip.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,22 +124,3 @@ eg https://www.myinstants.com/en/instant/ive-got-you-in-my-sight-52194/
 //loops can not overlap.
 
 
-//form validation function 
-
-function handleSubmit(event) {
-    event.preventDefault();
-    let p1 = form.elements['password'].value;
-    let p2 = form.elements['confirm-password'].value;
-
-    if (p1 !== p2) {
-        let errorDiv = document.getElementById('errors');
-        errorDiv.innerHTML = "<p>Please ensure your passwords match.</p>";
-        errorDiv.style.display = 'block';
-    } else {
-        console.log('Validation successful!');
-        form.submit();
-    }
-}
-
-let form = document.getElementById('registration-form');
-form.addEventListener('submit', handleSubmit);
