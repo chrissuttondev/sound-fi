803afce6f17c5569273258f3b37865c862231e03
chrissuttondev <>
2023-09-07 05:27:31 +0000

    removed onclick attribute from buttons and instead triggreing sound via event listener.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,109 +1,36 @@
 console.log("welcome to sound fi");
 
-//Javascritp function for audio pads.
-//https://www.w3schools.com/jsref/met_audio_play.asp
+//get audio form html
+let audioSamples = document.getElementsByTagName("audio");
+console.log(audioSamples); //console displays html collection
+//place audio in array
+let soundLibOne = Array.from(audioSamples);
+console.log(soundLibOne); //console displays array [o] through [0]
 
-const x = document.getElementById("kick");
+//function to play a sound via the pads
+let kick =document.getElementById("kick");//calls the sound (audio tag with id kick)
 
+//create function to play pad
 function playPadOne() {
-    x.play();
+    kick.play();
 }
-
-const y = document.getElementById("snare");
-
-function playPadTwo() {
-    y.play();
-}
-
-const b = document.getElementById("snare2");
-
-function playPadThree() {
-    b.play();
-}
-
-//
-const hh1 = document.getElementById("hh1");
-
-function playPadFour() {
-    hh1.play();
-}
-
-const hh2 = document.getElementById("hh2");
-
-function playPadFive() {
-    hh2.play();
-
-}
-
-const cymbol = document.getElementById("cymbol");
-
-function playPadSix() {
-    cymbol.play();
-}
-
-const vynil = document.getElementById("vynil");
-
-function playPadSeven() {
-    vynil.play();
-}
-
-const bleep1 = document.getElementById("bleep1");
-
-function playPadEight() {
-    bleep1.play();
-}
-
-const bleep2 = document.getElementById("bleep2");
-
-function playPadNine() {
-    bleep2.play();
-
-}
-
-const keys1 = document.getElementById("keys1");
-
-function playPadTen() {
-    keys1.play();
-}
-
-const keys2 = document.getElementById("keys2");
-
-function playPadEleven() {
-    keys2.play();
-}
-
-const keys3 = document.getElementById("keys3");
-
-function playPadTwelve() {
-    keys3.play();
-}
-
-//loop board
-
-const loop1 = document.getElementById("loop1");
-
-function playLoop1() {
-    loop1.play();
-}
-
-const loop2 = document.getElementById("loop2");
-
-function playLoop2() {
-    loop2.play();
-}
-
-const loop3 = document.getElementById("loop2");
-
-function playLoop3() {
-    loop3.play();
-}
+//declare function and assign to btn-1.
+let padOne = document.getElementById("btn-1");
+padOne.addEventListener('click', playPadOne) //events listener calls function upon click
 
 
-//audio section end
+
+
+
+
+
+
+
+
 
 /* js functionallity to add
 
-event listener. When user clicks a key on the keyboard a sound pad plays. 
+event listener. When user clicks a key on the keyboard a sound pad plays.
 Keys mapped
 z=1 x=2 c=3
 a=4 s=5 d=6
@@ -112,7 +39,7 @@ r=10 t=11 y=12
 
 eg. when key w is pressed playPadOne() function called
 
-/* Solution to problem described in the comment above founh here. 
+/* Solution to problem described in the comment above founh here.
 https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
 
 audio can played immiedatley from start even while playing back.
@@ -124,22 +51,3 @@ eg https://www.myinstants.com/en/instant/ive-got-you-in-my-sight-52194/
 //loops can not overlap.
 
 
-//form validation function 
-
-function handleSubmit(event) {
-    event.preventDefault();
-    let p1 = form.elements['password'].value;
-    let p2 = form.elements['confirm-password'].value;
-
-    if (p1 !== p2) {
-        let errorDiv = document.getElementById('errors');
-        errorDiv.innerHTML = "<p>Please ensure your passwords match.</p>";
-        errorDiv.style.display = 'block';
-    } else {
-        console.log('Validation successful!');
-        form.submit();
-    }
-}
-
-let form = document.getElementById('registration-form');
-form.addEventListener('submit', handleSubmit);
