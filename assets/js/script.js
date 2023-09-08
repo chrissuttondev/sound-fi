fbc2976b0867fe4a0880e240c178599a76002b77
chrissuttondev <>
2023-09-08 09:53:27 +0000

    added array for audio id tags and array for button id tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,36 @@
 console.log("welcome to sound fi");
 //Function play auio pads or the buttons and set them up to allow more flexibility and less repetaive code
+/* --------------------------------------------------------------
+//Get audio tags
+let audioTags = document.getElementsByTagName("audio");
+console.log(audioTags);
+
+//Get Buttons
+let buttons = document.getElementsByTag("button");
+console.log(buttons);
+
+
+Console logs audio tags. When same method used to get buttons console throws error 
+"script.js:9 Uncaught TypeError: document.getElementsByTag is not a function"
+------------------------------------------------------------*/
+//Sound coustructor ta access audio files instead???
+/*---------------------------------------------------------*/
+
+//Play audio via button Function
+
+
+//Audio ID Array to access audio html elements by ID
+const audioIds = ["kick", "snare", "snare2", "hh1", "hh2", "cymbol", "vynil",
+    "bleep1", "bleep2", "keys1", "keys2", "keys3"];
+
+    //Button ID Array to acces button html elements by ID
+const buttonIds = ["btn-1", "btn-2", "btn-3", "btn-4", "btn-5", "btn-6",
+"btn-7", "btn-8", "btn-9", "btn-10", "btn-11", "btn-12", ]   
+
+
+
+
+
 
 
 
@@ -17,10 +48,6 @@ console.log("welcome to sound fi");
 
 
 //funtion for loop pads. Event listeners. play on loop when clicked. play end of loop on next click.
-
-
-
-
 
 
 /* js functionallity to add
