b14ca90d4176354410664aabcc573e79ac46f1c1
chrissuttondev <>
2023-09-09 08:56:41 +0000

    removed kit2 audio files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,4 @@
-console.log("welcome to sound fi");
-//Function play auio pads or the buttons and set them up to allow more flexibility and less repetaive code
-/* --------------------------------------------------------------
-//Get audio tags
-//let audioTags = document.getElementsByTagName("audio");
-//console.log(audioTags);
-
-//Get Buttons
-let buttons = document.getElementsByTag("button");
-console.log(buttons);
-
-//Console logs audio tags. When same method used to get buttons console throws error 
-//"script.js:9 Uncaught TypeError: document.getElementsByTag is not a function"
-
-/*------------------------------------------------------------*/
-/*Sound coustructor ta access audio files instead? Less static html 
-access more audio files and give more variation for user. Could have different 
-kit or sound libraries. 
-
-css color scheme change upon loading to refelct different moods
-and keep user engaged. 
-
-Link below is a tutorial to a projec that uses sound constructor and offers user 3 different 
-kits / libaries.
-https://medium.com/@iminked/build-a-drum-machine-with-javascript-html-and-css-33a53eeb1f73
-Could expand on this idea and allow users to mix and match sounds and / or use math floor to select 
-kits at random.
----------------------------------------------------------*/
-
+console.log("Welcome to Sound-Fi");
 //Play audio via button function. Play audio on associated button when button clicked.
 function playPad(buttonId, audioId) {
     //get button and audio ids
@@ -48,6 +20,25 @@ const buttonIds = ["btn-1", "btn-2", "btn-3", "btn-4", "btn-5", "btn-6",
 const audioIds = ["kick", "snare", "snare2", "hh1", "hh2", "cymbol", "vynil",
     "bleep1", "bleep2", "keys1", "keys2", "keys3", "dr1", "dr2", "dr3"];
 
+/* key mapping
+event listener. When user clicks a key on the keyboard a sound pad plays.
+Keys mapped
+z=1 x=2 c=3
+a=4 s=5 d=6
+q=7 w=8 e=9
+r=10 t=11 y=12
+*/
+ //Keydown function map keys to buttons by id
+const keyMap = {
+    'z': 'btn-1'
+}
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+
+//variable
+document.addEventListener('keydown');
+
+ //source code;
+//www.section.io/engineering-education/keyboard-events-in-javascript/#javascript-keyboard-events
 //Set audio elements controls loop attribute to default as off when page loads
 for (let i = 0; i < audioIds.length; i++) {
     const audioId = audioIds[i];
@@ -58,40 +49,6 @@ for (let i = 0; i < audioIds.length; i++) {
     }
 
 }
-
-//method of playing audio could be more efiecent. Use JS to create and audio elemnent 
-//when a keyboard character, mouse or touch screen is interacted with by a user (the event).
-// set up event listener to capture the interaction
-//up the even occouring create and audio element to play desired audio file.
-//use appendChild to add audio element to the document.
-
-////// source code https://tinyurl.com/zdcrxr6a /////////
-
-var audioElement = document.createElement('audio');
-audioElement.setAttribute('src', 'audio.ogg');
-//how to point to source?
-function PlayAudio() {
-
-    audioElement.load;
-    audioElement.play();
-
-}
-
-
-
-
-
-////Loop false as default on page load alt method
-//The above solution is ok. Need to access all of the audio tags attibutes globally
-// const audioTags = documnet.getElementsByTagName(audio);
-// function loopOff (){
-//     if (audio){
-//        audio.loop = false;
-//     }
-// }
-
-//https://www.developerdrive.com/manipulating-html5s-native-audio-with-javascript/
-
 
 //Iterate through array(s) and set up event listener(s) 
 // For loop https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LMR101+2021_T1/courseware/73e9c0413ead4a21b389e33c77706102/4fe6474cab114387ad0e72bf7ec1c201/14?activate_block_id=block-v1%3ACodeInstitute%2BLMR101%2B2021_T1%2Btype%40html%2Bblock%40273b80fa87814494a107e8365ccc22fe    
@@ -124,7 +81,6 @@ function stopAudio(audioId) {
         audioElement.currentTime = 0;
     }
 }
-
 // Event listener for loop button
 document.getElementById("dr-lp-1").addEventListener('click', () => loopDrums('dr1'));
 document.getElementById("dr-lp-2").addEventListener('click', () => loopDrums('dr2'));
@@ -139,30 +95,38 @@ after and restart audio before it finishes playing. This will give more
 resonsivity and musicallity The will give the application functionality closer to
 real word audio sample players and enhance user experience*/
 
-/* key mapping
-event listener. When user clicks a key on the keyboard a sound pad plays.
-Keys mapped
-z=1 x=2 c=3
-a=4 s=5 d=6
-q=7 w=8 e=9
-r=10 t=11 y=12
 
-*/
 
-/*///////
+
+
+/*
 source code 
 https://www.section.io/engineering-education/keyboard-events-in-javascript/#javascript-keyboard-events
 
 
 */
-document.addEventListener('keydown', (event) => {
-    // triggers sound
-})
+//method of playing audio could be more efiecent. Use JS to create and audio elemnent 
+//when a keyboard character, mouse or touch screen is interacted with by a user (the event).
+// set up event listener to capture the interaction
+//up the even occouring create and audio element to play desired audio file.
+//use appendChild to add audio element to the document.
+////// source code https://tinyurl.com/zdcrxr6a /////////
 
-//end of source used//
 
-//array to hold keyboard value and audio pairs
+////Loop false as default on page load alt method
+//The above solution is ok. Need to access all of the audio tags attibutes globally
+//https://www.developerdrive.com/manipulating-html5s-native-audio-with-javascript/
 
-let keysTriggers = {
+/*Sound coustructor ta access audio files instead? Less static html 
+access more audio files and give more variation for user. Could have different 
+kit or sound libraries. 
 
-}
+css color scheme change upon loading to refelct different moods
+and keep user engaged. 
+
+Link below is a tutorial to a projec that uses sound constructor and offers user 3 different 
+kits / libaries.
+https://medium.com/@iminked/build-a-drum-machine-with-javascript-html-and-css-33a53eeb1f73
+Could expand on this idea and allow users to mix and match sounds and / or use math floor to select 
+kits at random.
+---------------------------------------------------------*/
